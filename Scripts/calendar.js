4f06d5f101acd763905aa27091046b73bb13549b
Diego Alexander Ramirez Acosta <alexander221b@hotmail.com>
2021-11-23 21:47:39 -0500

    cargue front

---
+++ Scripts/calendar.js
@@ -0,0 +1,60 @@
+
+
+//Configura y crea un calendario js
+function createCalendar(jsonEvents){
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay'
+      },
+      initialDate: '2020-09-12',
+      timeZone: 'local',
+      navLinks: true, // can click day/week names to navigate views
+      selectable: true,
+      selectMirror: true,
+      select: function(arg) {
+        var title = prompt('Event Title:');
+        console.log("Datos para crear evento: ");
+        console.log(arg);
+        if (title) {
+          //Crea un evento
+          createEvent(calendar, arg, title);
+        }
+        
+
+        calendar.unselect()
+      },
+      eventClick: function(info) {
+        if (confirm('Are you sure you want to delete this event?')) {
+          var eventObj = info.event;
+            if (eventObj.id) {
+                //Quita el evento del calendario
+                var event = calendar.getEventById(eventObj.id);
+                event.remove();
+                //Quita el evento de la api
+                removeEventApi(eventObj.id); 
+            }
+        }
+      },
+      editable: true,
+      dayMaxEvents: true, // allow "more" link when too many events
+      events: jsonEvents,
+      eventDrop: function(event) {
+        arg = event.event;
+        updateEvent(arg);
+        //console.log(arg.title);
+
+      }
+    });
+    
+    calendar.render();
+    calendar.today();
+    //calendar.today();
+    //console.log(fechaInicial);
+
+
+  }
+
+
---
+++ Scripts/events.js
@@ -0,0 +1,117 @@
+const urlEvents = 'https://calendarrest.azurewebsites.net/api/eventos/';
+
+//Trae la data del servicio rest
+function requestEvents(){
+        fetch(urlEvents, {
+          method: "GET",
+          headers: {"Content-type": "application/json;charset=UTF-8"}
+        })
+        .then(response => response.json())  
+        .then(data => {
+        	     //Si hay respuesta pasa la data  
+        	     //crea los eventos en el calendario
+                 loadEvents(data);
+              }
+
+        )
+        .catch(err => err);
+}
+
+//Crea un objeto Json con los eventos de la base de datos
+function loadEvents(data){
+	var jsonEvents = [];
+    for (let event of data) {
+    	var feed = {
+    		    'id': event.id,
+    				'title': event.title, 
+    				'start': event.start, 
+    				'end': event.end,
+            'allDay':event.allday
+            
+    			};
+		jsonEvents.push(feed);
+    }
+
+    console.log("Eventos cargados de la base de datos:");
+    console.log(jsonEvents);
+
+    //Crea el calendario con el objeto json
+    createCalendar(jsonEvents);
+}   
+
+
+//Crea un evento en la base de datos y con la respuesta en el calendario usando
+// su id de evento proveniente de la base de datos
+function createEvent(calendar, arg, title){
+    // data to be sent to the POST request
+
+    let data = {
+      title: title,
+      start: arg.start, 
+      end: arg.end,
+      allday: arg.allDay
+     }
+
+     console.log(data);
+     
+    fetch(urlEvents, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {"Content-type": "application/json; charset=UTF-8"}
+    })
+    .then(response => response.json()) 
+    .then(data => {
+      //crea el evento con el id asignado en base de datos data.id
+      calendar.addEvent({
+            id: data.id,
+            title: title,
+            start: arg.start,
+            end: arg.end,
+            allday: arg.allDay
+          })
+
+    })
+    .catch(err => console.log(err));
+}
+
+
+function removeEventApi(id){
+   //Remueve el evento de la api
+    fetch(urlEvents + id, {
+      method: 'DELETE',
+    })
+    .then(res => res.text()) // or res.json()
+    .then(res => console.log(res));
+}
+
+function updateEvent(arg){
+    id = arg.id;
+    // data to be sent to the POST request
+    let data = {
+      id:id,
+      title:arg.title,
+      start: arg.start, 
+      end: arg.end,
+      allday: arg.allDay
+    };
+     
+     const putMethod = {
+       method: 'PUT', // Method itself
+       headers: {
+        'Content-type': 'application/json' // Indicates the content 
+       },
+       body: JSON.stringify(data) // We send data in JSON format
+      };
+
+      // make the HTTP put request using fetch api
+      fetch(urlEvents + id, putMethod)
+      .then(response => response.json())
+      .then(data => console.log(data)) // Manipulate the data retrieved back, if we want to do something with it
+      .catch(err => console.log(err)); // Do something with the error
+
+
+}
+
+
+
+
